9384e189fa091174132f153cafb5d4b43fe831d6
Baby Pepper <royxagain@gmail.com>
2016-05-20 11:54:06 -0700

    fix db things

--- db/migrate/20160518063126_create_students.rb
+++ db/migrate/20160518063126_create_students.rb
@@ -10,11 +10,6 @@ class CreateStudents < ActiveRecord::Migration
     end
     add_index :students, :email
 
-    create_table :courses_students, :id => false do |t|
-      t.integer :course_id
-      t.integer :student_id
-    end
-
     add_index :courses_students, [:course_id, :student_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518064216) do
+ActiveRecord::Schema.define(version: 20160520183006) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "dept"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160518064216) do
     t.string   "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "student_id"
   end
 
   create_table "roles", force: :cascade do |t|
